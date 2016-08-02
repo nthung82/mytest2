6e8ef42a2bfe205ee00d71379157cf2386dc9e50
Nguyen Thanh Hung <nthung82@gmail.com>
2016-08-02 22:16:24 +0700

    sdfdsfsdfs

--- src/main/java/com/journaldev/spring/controller/HomeController.java
+++ src/main/java/com/journaldev/spring/controller/HomeController.java
@@ -22,7 +22,7 @@ public class HomeController {
 	
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String home(Locale locale, Model model, HttpServletRequest r) {
-		Date date = new Date();///////////////11111111111111hungd02cnttfsdfsdfsdfs
+		Date date = new Date();///////////////11111111111111hungd0sdfsdfsdfs2cnttfsdfsdfsdfs
 //vxcvxcvxcv1sdfdsfsdfs
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
